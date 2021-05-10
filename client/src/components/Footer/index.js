b6a7d842dd453c14470af7cdc17837977f402779
bvictorb <victory-boucher@hotmail.com>
2021-05-10 19:36:06 +0200

    created footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,34 +6,50 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <img 
-          className={styles.logo} 
-          src='/images/dept-white.png' 
-          alt='Dept logo' 
-        />
-        <div className={styles.linkContainer}>
-          {links.map((link, index) => <a 
-            key={index}
-            className={styles.link}
-            target='_blank' 
-            href={`https://www.deptagency.com/${link}/`}
-          >
-            {link}
-          </a>)}
-          <div className={styles.info}>
-            <p>Chamber of Commerce: 63464101</p>
-            <p>VAT: NL 8552.47.502.B01</p>
-            <p>Terms and conditions</p>
+        <div className={styles.topContainer}>
+          <img 
+            className={styles.logo} 
+            src='/images/dept-white.png' 
+            alt='Dept logo' 
+          />
+          <div className={styles.linksContainer}>
+            {links.map((link, index) => <a 
+              key={index}
+              className={styles.link}
+              target='_blank' 
+              href={`https://www.deptagency.com/${link}/`}
+            >
+              {link}
+            </a>)}
+          </div>
+          <div className={styles.icons}>
+            <a target='_blank' href='https://www.facebook.com/DeptAgency/'>
+              <img src='/icons/facebook.png' alt='Facebook icon' />
+            </a>
+            <a target='_blank' href='https://twitter.com/deptagency'>
+              <img src='/icons/twitter.png' alt='Twitter icon' />
+            </a>
+            <a target='_blank' href='https://www.instagram.com/deptagency/'>
+              <img src='/icons/instagram.png' alt='Instagram icon' />
+            </a>
           </div>
         </div>
+        <div className={styles.bottomContainer}>
+          <p className={styles.info}>Chamber of Commerce: 63464101</p>
+          <p className={styles.info}>VAT: NL 8552.47.502.B01</p>
+          <p className={styles.info}>Terms and conditions</p>
+          <p className={styles.copyright}>© 2021 Dept Agency</p>
+        </div>
       </div>
-      <button 
-        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-      >
-        Scroll to the top
-      </button>
     </footer>
   )
 }
+
+{/* <button 
+  className={styles.scrollButton}
+  onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+  >
+  Scroll to the top
+</button> */}
 
 export default Footer
